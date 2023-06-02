5940bdd1a76d48fd6273bf01310a2dc2f6e3eccf
redblood639 <albinorabbithunter@gmail.com>
2023-06-02 05:55:09 -0400

    fix : update

--- src/utility/connectors.ts
+++ src/utility/connectors.ts
@@ -3,13 +3,13 @@ import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 import { WalletLinkConnector } from "@web3-react/walletlink-connector";
 
 const CoinbaseWallet = new WalletLinkConnector({
-	url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+	url: `https://mainnet.infura.io/v3/${process.env.PUBLIC_NEXT_INFURA_KEY}`,
 	appName: "Web3-react Demo",
 	supportedChainIds: [1, 3, 4, 5, 42],
 });
 
 const WalletConnect = new WalletConnectConnector({
-	rpcUrl: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+	rpc: `https://mainnet.infura.io/v3/${process.env.PUBLIC_NEXT_INFURA_KEY}`,
 	bridge: "https://bridge.walletconnect.org",
 	qrcode: true,
 });
